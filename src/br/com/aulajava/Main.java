26a6b508d73faa53cc2a16e12a34ad1fbee4a17a
unknown <oliveirapereiraandre@gmail.com>
2020-11-09 21:59:56 -0300

    MENSAGEM

---
+++ src/br/com/aulajava/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.aulajava;
+
+/**
+ * @author andre
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
